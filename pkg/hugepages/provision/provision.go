246ada6cdafeb76927959b1cbc7208413a092794
Francesco Romani <fromani@redhat.com>
2025-10-16 09:28:00 +0200

    hugepage: add support to provision hugepages
    add optional flow to support the provisioning of hugepages.
    Initially, we only support dynamic provisioning at runtime,
    which is the discouraged approach but is much simpler to
    implement as it doesn't require a node reboot.
    
    Later we will add support to generate the kernel commandline
    to provision hugepages at node start, reusing the API
    and the infrastructure added here.
    
    Provisioning support makes sense to complete the functionality,
    so this driver becomes capable to enumarate, allocate and provision
    hugepages.
    
    Online on-demand provisioning is intentionally not (yet?)
    supported: all hugepages need to be provisioned before use,
    this feature just makes it easier to do, reducing or avoiding
    the need to use external tools/scripts.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

---
+++ pkg/hugepages/provision/provision.go
@@ -0,0 +1,104 @@
+package provision
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/go-logr/logr"
+	ghwopt "github.com/jaypipes/ghw/pkg/option"
+	ghwtopology "github.com/jaypipes/ghw/pkg/topology"
+
+	"sigs.k8s.io/yaml"
+
+	apiv0 "github.com/ffromani/dra-driver-memory/pkg/hugepages/provision/api/v0"
+)
+
+func ReadConfiguration(source string) (apiv0.HugePageProvision, error) {
+	if source == "-" {
+		return readConfigurationFrom(os.Stdin)
+	}
+	src, err := os.Open(source)
+	if err != nil {
+		return apiv0.HugePageProvision{}, err
+	}
+	defer src.Close()
+	return readConfigurationFrom(src)
+}
+
+func RuntimeHugepages(logger logr.Logger, hpp apiv0.HugePageProvision, sysRoot string) error {
+	logger.V(2).Info("start provisioning hugepages", "groups", len(hpp.Spec.Pages))
+	defer logger.V(2).Info("done provisioning hugepages", "groups", len(hpp.Spec.Pages))
+
+	sysinfo, err := ghwtopology.New(ghwopt.WithChroot(sysRoot))
+	if err != nil {
+		return err
+	}
+
+	for _, conf := range hpp.Spec.Pages {
+		var err error
+
+		if conf.Node != nil {
+			logger.V(2).Info("provisioning pages", "numaNode", *conf.Node, "count", conf.Count, "size", conf.Size)
+			err = provisionOnNode(logger, int(*conf.Node), int(conf.Count), conf.Size, sysRoot)
+		} else if len(sysinfo.Nodes) == 1 {
+			logger.V(2).Info("provisioning pages", "numaNode", 0, "count", conf.Count, "size", conf.Size)
+			err = provisionOnNode(logger, 0, int(conf.Count), conf.Size, sysRoot)
+		} else {
+			logger.V(2).Info("splitting pages", "count", conf.Count, "NUMACount", len(sysinfo.Nodes))
+			err = provisionOnMultiNode(logger, len(sysinfo.Nodes), int(conf.Count), conf.Size, sysRoot)
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func provisionOnMultiNode(logger logr.Logger, numaNodeCount, hpCount int, hpSize apiv0.HugePageSize, sysRoot string) error {
+	extra := hpCount % numaNodeCount
+	perNode := hpCount / numaNodeCount
+
+	// we choose to move excess pages on numa node 0 because this is the most common observed practice
+	err := provisionOnNode(logger, 0, perNode+extra, hpSize, sysRoot)
+	if err != nil {
+		return err
+	}
+	for numaNode := 1; numaNode < numaNodeCount; numaNode++ {
+		err = provisionOnNode(logger, numaNode, perNode, hpSize, sysRoot)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func provisionOnNode(logger logr.Logger, numaNode, hpCount int, apiHpSize apiv0.HugePageSize, sysRoot string) error {
+	// this is done too late, we should have proper validation and API translation but good enough for starters.
+	hpSize, err := apiv0.ValidateHugePageSize(apiHpSize)
+	if err != nil {
+		return err
+	}
+	hpPath := filepath.Join(sysRoot, "sys", "devices", "system", "node", fmt.Sprintf("node%d", numaNode), "hugepages", "hugepages-"+string(hpSize), "nr_hugepages")
+	logger.V(4).Info("writing on sysfs", "path", hpPath)
+	dst, err := os.OpenFile(hpPath, os.O_WRONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+	_, err = dst.WriteString(strconv.Itoa(hpCount))
+	return fmt.Errorf("failed to write on %q: %w", hpPath, err)
+}
+
+func readConfigurationFrom(r io.Reader) (apiv0.HugePageProvision, error) {
+	hpp := apiv0.HugePageProvision{}
+	data, err := io.ReadAll(r)
+	if err != nil {
+		return hpp, err
+	}
+	err = yaml.Unmarshal(data, &hpp)
+	return hpp, err
+}
