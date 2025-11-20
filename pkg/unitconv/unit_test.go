687a5f5f6a06f158733e91fd0e2a8a6a7b875c61
Francesco Romani <fromani@redhat.com>
2025-11-20 10:05:22 +0100

    chore: unitconv: improve test coverage
    add coverage with trivial extesion
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/unitconv/unit_test.go
+++ pkg/unitconv/unit_test.go
@@ -27,9 +27,11 @@ func TestSizeToStringRoundTrip(t *testing.T) {
 	type testcase struct {
 		sval string
 		uval uint64
+		fail bool
 	}
 
 	testcases := []testcase{
+		// good cases, add them at the bottom of the section
 		{
 			sval: "4k",
 			uval: 4 * 1024,
@@ -46,14 +48,118 @@ func TestSizeToStringRoundTrip(t *testing.T) {
 			sval: "1g",
 			uval: 1024 * 1024 * 1024,
 		},
+		// bad cases, add them at the bottom of the section
+		{
+			sval: "",
+			fail: true,
+		},
+		{
+			sval: "-1",
+			fail: true,
+		},
+		{
+			sval: "g",
+			fail: true,
+		},
+		{
+			sval: "Kk",
+			fail: true,
+		},
+		{
+			sval: "8b",
+			fail: true,
+		},
+		{
+			sval: "42xb",
+			fail: true,
+		},
+		{
+			sval: "1pb",
+			fail: true,
+		},
 	}
 
 	for _, tcase := range testcases {
 		t.Run(fmt.Sprintf("%s=%d", tcase.sval, tcase.uval), func(t *testing.T) {
 			ugot, err := MinimizedStringToSizeInBytes(tcase.sval)
+			if tcase.fail {
+				require.Error(t, err)
+				return
+			}
 			require.NoError(t, err)
 			require.Equal(t, ugot, tcase.uval)
 			sgot := SizeInBytesToMinimizedString(ugot)
+			require.Equal(t, sgot, tcase.sval)
+		})
+	}
+}
+
+func TestCGroupStringToSizeRoundTrip(t *testing.T) {
+	type testcase struct {
+		sval string
+		uval uint64
+		fail bool
+	}
+
+	testcases := []testcase{
+		// good cases, add them at the bottom of the section
+		{
+			sval: "4KB",
+			uval: 4 * 1024,
+		},
+		{
+			sval: "64KB",
+			uval: 64 * 1024,
+		},
+		{
+			sval: "2MB",
+			uval: 2 * 1024 * 1024,
+		},
+		{
+			sval: "1GB",
+			uval: 1024 * 1024 * 1024,
+		},
+		// bad cases, add them at the bottom of the section
+		{
+			sval: "",
+			fail: true,
+		},
+		{
+			sval: "-1",
+			fail: true,
+		},
+		{
+			sval: "GB",
+			fail: true,
+		},
+		{
+			sval: "KKB",
+			fail: true,
+		},
+		{
+			sval: "8B",
+			fail: true,
+		},
+		{
+			sval: "42XB",
+			fail: true,
+		},
+		{
+			sval: "1PB",
+			fail: true,
+		},
+	}
+
+	for _, tcase := range testcases {
+		t.Run(fmt.Sprintf("%s=%d", tcase.sval, tcase.uval), func(t *testing.T) {
+			ugot, err := CGroupStringToSizeInBytes(tcase.sval)
+			if tcase.fail {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, ugot, tcase.uval)
+			sgot := SizeInBytesToCGroupString(ugot)
 			require.Equal(t, sgot, tcase.sval)
 		})
 	}
